d2e05040f79130f22bc808f26e6e1d20f9f7d2d6
opietrzakbdr <oliwia.pietrzak.home@gmail.com>
2024-06-23 21:44:31 +0200

    Proejtk

---
+++ tests/testMarketingScripts.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+
+
+// sprawdza czy przy zaakceptowanych cookiesach wysyla request (ma wysyłać)
+test('testCheckAdServerCookieIfAllowed', async ({ page }) => {
+  await page.goto('https://home.pl/');
+  const requestPromise = page.waitForRequest('https://ias.home.pl/t/web?identity=COOKIE');
+  await page.getByRole('button', { name: 'AKCEPTUJ WSZYSTKIE' }).click();
+  const request = await requestPromise;
+}); 
+
+
+// sprawdza czy przy odrzuconych cookiesach wysyłany request (ma nie wysyłać)
+test('testCheckAdServerCookieIfNotAllowed', async ({ page }) => {
+  await page.goto('https://home.pl/');
+  const requestPromise = page.waitForRequest('https://ias.home.pl/t/web?identity=COOKIE');
+  await page.getByRole('button', { name: 'ODRZUĆ WSZYSTKIE' }).click();
+  const request = await requestPromise;
+}); 
+
+
+// testCheckAnalytic zostawiam chociaż nie działa prawidłowo - wyświetla timeout - gdy na home.pl odrzucimy pliki 
+// cookie powinien i tak ładować się skrypt Analyticsa (jest on zaliczany do katrgorii "Niezbędne" w płachcie cookie), 
+// test nie przechodzi niezależnie od tego czy pliki cookie odrzucimy czy zaakceptujemy
+
+test('testCheckAnalytics', async ({ page }) => {
+  await page.goto('https://home.pl/');
+  const requestPromise = page.waitForRequest(request =>
+    request.url() === 'https://www.google-analytics.com/analytics.js' && request.method() === 'GET',
+  );
+  await page.getByRole('button', { name: 'ODRZUĆ WSZYSTKIE' }).click();
+  const request = await requestPromise;
+});
+
---
+++ tests/testPrices.spec.ts
@@ -0,0 +1,77 @@
+import { test, expect } from '@playwright/test';
+
+test('testPricesMainPage', async ({ page }) => {
+    await page.goto('https://home.pl/');
+    await page.getByRole('button', { name: 'ODRZUĆ WSZYSTKIE' }).click();
+    await expect(page.getByRole('link', { name: 'pl-0 6,14 zł / rok Polecamy' })).toContainText('6,14 zł');
+    await expect(page.getByRole('link', { name: 'com-1 56,58 zł / rok' })).toContainText('56,58 zł');
+    await expect(page.getByRole('link', { name: 'eu-2 29,52 zł / rok' })).toContainText('29,52 zł');
+  });
+
+test('testPricePlDomainPage', async ({ page }) => {
+    await page.goto('https://home.pl/');
+    await page.getByRole('button', { name: 'ODRZUĆ WSZYSTKIE' }).click();
+    await page.getByRole('link', { name: 'pl-0 6,14 zł / rok Polecamy' }).click();
+    await expect(page.getByRole('main')).toContainText('6,14 zł');
+});
+
+test('testPriceEuDomainPage', async ({ page }) => {
+    await page.goto('https://home.pl/');
+    await page.getByRole('button', { name: 'ODRZUĆ WSZYSTKIE' }).click();
+    await page.getByRole('link', { name: 'eu-2 29,52 zł / rok' }).click();
+    await expect(page.getByRole('main')).toContainText('29,52 zł');
+});
+
+test('testPriceHostingPage', async ({ page }) => {
+    await page.goto('https://home.pl/');
+    await page.getByRole('button', { name: 'ODRZUĆ WSZYSTKIE' }).click();
+    await page.getByRole('link', { name: 'Hosting WWW' }).click();
+    await expect(page.getByTestId('product-box-price-price')).toContainText('49,08 zł');
+    await expect(page.getByTestId('product-box-price-price')).toContainText('61,38 zł');
+    await expect(page.getByTestId('product-box-price-price')).toContainText('184,50 zł');
+    await expect(page.getByTestId('product-box-price-price')).toContainText('492,00 zł');
+    // await expect(page.locator('#szczegoly')).toContainText('49,08 zł');
+  });
+
+// await expect(page.locator('#szczegoly')).toContainText('49,08 zł');
+//  await expect(page.getByRole('link', { name: 'pl-0 6,14 zł / rok Polecamy' })).toBeVisible();
+
+//  await page.locator('css=#order_bill_address_attributes_zipcode').fill('70-123');
+
+// await expect(page.getByRole('heading', { name: 'Order placed successfully' })).toBeVisible();
+
+// await expect(page.getByRole('link', { name: 'pl-0 6,14 zł / rok Polecamy' })).toBeVisible();
+
+// await expect(page.getByRole('main')).toContainText('6,14 zł');
+
+
+// await page.getByRole('link', { name: 'pl-0 6,14 zł / rok Polecamy' }).click();
+// await expect(page.getByRole('main')).toContainText('Cena od 6,14 zł brutto / rok');
+// await page.getByText('Cena od 6,14 zł brutto / rok').click({
+//   button: 'right'
+// });
+// await page.locator('#gatsby-focus-wrapper').click();
+// await page.getByText('Cena od 6,14 zł brutto / rok').click();
+// });
+
+// await page.getByRole('link', { name: 'Domeny internetowe' }).click();
+// await page.getByRole('link', { name: 'Domena .pl' }).click({
+//   button: 'right'
+// });
+// await page.getByRole('link', { name: 'Domena .pl' }).click();
+// }); 
+
+
+
+// test('testHostingPrice', async ({ page }) => {
+//     await page.goto('https://home.pl/');
+//     await page.getByRole('button', { name: 'ODRZUĆ WSZYSTKIE' }).click();
+//     await page.getByRole('link', { name: 'Hosting WWW' }).click();
+//     await page.getByTestId('product-box-price-price').toContainText('49,08 zł');
+//     await page.getByTestId('product-box-price-price').toContainText('61,38 zł');
+//     await page.getByTestId('product-box-price-price').toContainText('184,50 zł');
+//     await page.getByTestId('product-box-price-price').toContainText('492,00 zł');
+//     // await expect(page.locator('#szczegoly')).toContainText('49,08 zł');
+// await expect(page.locator('#szczegoly')).toContainText('49,08 zł');
+//   });
+
