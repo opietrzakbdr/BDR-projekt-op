153691d8dcdc7be387b9c3cb87903cd52349cdb5
opietrzakbdr <oliwia.pietrzak.home@gmail.com>
2024-06-23 23:06:36 +0200

    Projekt

--- e2e/testAX.spec.ts
+++ e2e/testAX.spec.ts
@@ -1,10 +1,12 @@
+import { fakerPL } from '@faker-js/faker';
 import { test, expect } from '@playwright/test';
 
 test('testAX', async ({ page }) => {
+  test.setTimeout(120000);
   await page.goto('https://home.pl/');
   await page.getByRole('button', { name: 'ODRZUĆ WSZYSTKIE' }).click();
   await page.getByRole('textbox', { name: 'Wyszukaj domenę lub produkt' }).click();
-  await page.getByRole('textbox', { name: 'Wyszukaj domenę lub produkt' }).fill('jdwnxwnax');
+  await page.getByRole('textbox', { name: 'Wyszukaj domenę lub produkt' }).fill(fakerPL.lorem.lines(1));
   await page.getByRole('button', { name: 'Wyszukaj' }).click();
   await page.locator('#domain-search-transfer').getByRole('button', { name: 'Dodaj do koszyka' }).click();
   await page.locator('.sc-1enuft9-0').click();
