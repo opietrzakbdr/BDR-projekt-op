862b14397b6cafa44abf273736f96b59e66ecb6c
opietrzakbdr <oliwia.pietrzak.home@gmail.com>
2024-06-24 11:54:57 +0200

    Projekt

--- e2e/testAX.spec.ts
+++ e2e/testAX.spec.ts
@@ -1,10 +1,17 @@
 import { fakerPL } from '@faker-js/faker';
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
+
+
+async function goToHomepl(page: Page, cookieDeclaration: string) {
+  await page.goto('https://home.pl');
+  page.getByRole('button', { name: cookieDeclaration }).click();
+  await expect(page.getByRole('link', { name: 'Logo home.pl' })).toBeVisible();
+}
+
 
 test('testAX', async ({ page }) => {
   test.setTimeout(120000);
-  await page.goto('https://home.pl/');
-  await page.getByRole('button', { name: 'ODRZUĆ WSZYSTKIE' }).click();
+  await goToHomepl(page, 'ODRZUĆ WSZYSTKIE')
   await page.getByRole('textbox', { name: 'Wyszukaj domenę lub produkt' }).click();
   await page.getByRole('textbox', { name: 'Wyszukaj domenę lub produkt' }).fill(fakerPL.lorem.lines(1));
   await page.getByRole('button', { name: 'Wyszukaj' }).click();
--- e2e/testPrices.spec.ts
+++ e2e/testPrices.spec.ts
@@ -1,23 +1,28 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
+
+
+async function goToHomepl(page: Page, cookieDeclaration: string) {
+  await page.goto('https://home.pl');
+  page.getByRole('button', { name: cookieDeclaration }).click();
+  await expect(page.getByRole('link', { name: 'Logo home.pl' })).toBeVisible();
+}
+
 
 test('testPricesMainPage', async ({ page }) => {
-    await page.goto('https://home.pl/');
-    await page.getByRole('button', { name: 'ODRZUĆ WSZYSTKIE' }).click();
+    await goToHomepl(page, 'ODRZUĆ WSZYSTKIE')
     await expect(page.getByRole('link', { name: 'pl-0 6,14 zł / rok Polecamy' })).toContainText('6,14 zł');
     await expect(page.getByRole('link', { name: 'com-1 56,58 zł / rok' })).toContainText('56,58 zł');
     await expect(page.getByRole('link', { name: 'eu-2 29,52 zł / rok' })).toContainText('29,52 zł');
   });
 
 test('testPricePlDomainPage', async ({ page }) => {
-    await page.goto('https://home.pl/');
-    await page.getByRole('button', { name: 'ODRZUĆ WSZYSTKIE' }).click();
+    await goToHomepl(page, 'ODRZUĆ WSZYSTKIE')
     await page.getByRole('link', { name: 'pl-0 6,14 zł / rok Polecamy' }).click();
     await expect(page.getByRole('main')).toContainText('6,14 zł');
 });
 
 test('testPriceEuDomainPage', async ({ page }) => {
-    await page.goto('https://home.pl/');
-    await page.getByRole('button', { name: 'ODRZUĆ WSZYSTKIE' }).click();
+    await goToHomepl(page, 'ODRZUĆ WSZYSTKIE')
     await page.getByRole('link', { name: 'eu-2 29,52 zł / rok' }).click();
     await expect(page.getByRole('main')).toContainText('29,52 zł');
 });
